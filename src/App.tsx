aa8042ae3e2af9cd210a43c7d86bff88aea27108
Anurup Chandra Mondal <anurup.mondal75@gmail.com>
2025-11-24 05:20:23 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 // import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import { ThemeProvider } from './context/ThemeContext';
-import Layout from './layout/Layout';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import ScrollToTop from './components/common/ScrollToTop';
-import CursorBackground from './components/common/CursorBackground';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
+import { ThemeProvider } from "./context/ThemeContext";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/common/ScrollToTop";
+import CursorBackground from "./components/common/CursorBackground";
 
 // Component to handle 404.html redirects for GitHub Pages SPA
 function RedirectHandler() {
@@ -16,13 +22,13 @@ function RedirectHandler() {
   useEffect(() => {
     // Check if we have a redirect query parameter (from 404.html)
     const queryParams = new URLSearchParams(location.search);
-    const redirectPath = queryParams.get('/');
-    
+    const redirectPath = queryParams.get("/");
+
     if (redirectPath) {
       // Decode the path and navigate to it
       const decodedPath = redirectPath
-        .replace(/~and~/g, '&')
-        .replace(/%26/g, '&');
+        .replace(/~and~/g, "&")
+        .replace(/%26/g, "&");
       navigate(decodedPath, { replace: true });
     }
   }, [location.search, navigate]);
